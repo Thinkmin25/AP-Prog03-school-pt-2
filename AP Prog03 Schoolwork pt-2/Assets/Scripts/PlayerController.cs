d0bd0d36e429d279216786f46bc5aa2154ba3576
papadoal <papadoal@OA-S141-10>
2025-11-17 15:58:02 -0500

    Jumping mildly works now, but next Journal is coming up, so the jumping will need a lot of improvements. The structure is here, it just needs fine tuning nad bug fixing.

--- AP Prog03 Schoolwork pt-2/Assets/Scripts/PlayerController.cs
+++ AP Prog03 Schoolwork pt-2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Vector2 playerInput = Vector2.zero;
     [SerializeField] bool lastDirLeft = false;
 
+    [SerializeField] float apexHeight = 3;
+    [SerializeField] float apexTime = 0.8f;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpVel;
+    [SerializeField] float terminalFallSpeed = -0.2f;
+    [SerializeField] float currentTime = 0;
+    [SerializeField] Vector2 lastPos = Vector2.zero;
+    [SerializeField] float startingJumpPos = 0;
+    [SerializeField] bool jumping = false;
+    [SerializeField] BoxCollider2D boxCollider;
+
     public enum FacingDirection
     {
         left, right
@@ -15,6 +26,10 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        lastPos = rb.position;
+        gravity = -2 * apexHeight / Mathf.Pow(apexTime, 2);
+        jumpVel = 4 * apexHeight / apexTime;
     }
 
     void Update()
@@ -23,7 +38,7 @@ public class PlayerController : MonoBehaviour
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
 
-        playerInput = Vector2.zero;
+        playerInput.x = 0;
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -33,20 +48,39 @@ public class PlayerController : MonoBehaviour
         {
             playerInput.x += moveSpeed;
         }
+
+        if (IsGrounded())
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                playerInput.y += jumpVel;
+                jumping = true;
+            }
+            else
+            {
+                jumping = false;
+            }
+        }
+        else
+        {
+            playerInput.y += Mathf.Clamp(gravity, terminalFallSpeed, 10);
+        }
         
-        MovementUpdate(playerInput);
         IsWalking();
         GetFacingDirection();
+        MovementUpdate(playerInput);
+        
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        rb.position += playerInput * Time.deltaTime;
+        lastPos = rb.position;
+        rb.position = (rb.position + playerInput * Time.deltaTime);
     }
 
     public bool IsWalking()
     {
-        if (rb.linearVelocity != Vector2.zero)
+        if (rb.position + playerInput != lastPos)
         {
             return true;
         }
@@ -54,8 +88,17 @@ public class PlayerController : MonoBehaviour
     }
     public bool IsGrounded()
     {
-        if (rb.linearVelocityY == 0)
+        RaycastHit2D cast = Physics2D.Raycast(rb.position, -transform.up, playerInput.y - gravity);
+        if (cast && playerInput.y <= 0)
         {
+            Debug.Log(cast.distance + " " + cast.rigidbody);
+
+            if (playerInput.y != 0)
+            {
+                rb.position -= new Vector2(0, cast.distance - boxCollider.size.y / 2);
+            }
+
+            playerInput.y = 0;
             return true;
         }
         else return false;
